c6a3f5e2d51c113061ca9bbd5c29e4f4a038e81e
gustaF <alipioman41@gmail.com>
2023-08-24 08:42:11 -0500

    'avanzando-la-parte-interactiva'

--- ReactQuizz/src/App.jsx
+++ ReactQuizz/src/App.jsx
@@ -1,34 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+  import {useState , useEffect} from 'react'
+import './App.css'    
 
+const URL = "https://restcountries.com/v3.1/all"
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries,setCountries] = useState([])
+  const [countriesSelected , setCountriesSelected] = useState([])
+  const [statusOptions ,setStatusOptions] = useState({statusA:false,statusB:false,statusC:false,statusD:false})
+  
+
+
+  useEffect(()=>{
+    const getCountries = async()=>{
+      const response = await fetch(URL)
+      const res = await response.json()
+      setCountries(res)
+    }
+    getCountries()
+    //obteniendo un array con 4 paises diferentes
+  },[])
+
+ 
+  
+  useEffect(()=>{
+    
+    setCountriesSelected([])
+      //FALTA PONER UN CONDICIONAL PARA QUE NO TOQUEN PAISES REPETIDOS ,PUEDE SER PONERLOS EN UN SET O CON UN FOR COMPARAR     
+       let auxArray = []
+      
+        while(auxArray.length<4){
+          auxArray = [ countries[Math.floor(Math.random()*countries.length)],...auxArray]  
+                 }
+          setCountriesSelected(auxArray)
+          console.log(countriesSelected)
+      
+       
+
+
+    } ,[statusOptions])
+     
+    
+
+   const clicOptionA = ()=>{
+    setStatusOptions({statusA:true,statusB:false,statusC:false,statusD:false})
+    
+   }
+
+   const clicOptionB = ()=>{
+    setStatusOptions({statusA:false,statusB:true,statusC:false,statusD:false})
+    console.log('option B choosed')
+   }
+   const clicOptionC = ()=>{
+    setStatusOptions({statusA:false,statusB:false,statusC:true,statusD:false})
+    console.log('option C choosed')
+   }
+   const clicOptionD = ()=>{
+    setStatusOptions({statusA:false,statusB:false,statusC:false,statusD:true})
+    console.log('option D choosed')
+   }
+
 
   return (
+    
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    {/*AL INTENTAR ACCEDER A LAS PROPIEDADES DE LOS PAISES ME APARECIA EL ERROR >CANNOT ACCESS PROPERTIES OF
+       UNDEFINED CADA VEZ QUE RECARGABA LA PAGINA,     PARA SOLUCIONARLO  TUVE QUE AGREGAR EL OPERADOR '?'
+      ANTES DE QUERER ACCEDER A ESA PROPIEDAD */}
+      
+      
+      { Array.isArray(countriesSelected)
+           ?  <section>
+           <strong>COUNTRY QUIZ</strong>
+            <p><strong>{countriesSelected[Math.floor(Math.random()*4)]?.capital[0]}</strong> ES LA CAPITAL DE :</p>
+            <br/>
+            
+            <button
+            className='OPCION A'
+            onClick={clicOptionA}
+            >OPTION A : {countriesSelected[0]?.name?.common}
+             </button>
+     
+             <br/>
+            
+             <button
+             className='OPCION B'
+             onClick={clicOptionB}
+               >OPTION B : {countriesSelected[1]?.name?.common}
+               </button>
+             <br/>
+     
+             <button
+             className='OPCION C'
+             onClick={clicOptionC}
+             >OPTION C : {countriesSelected[2]?.name?.common}</button>
+             <br/>
+             
+             <button  className='OPCION D' 
+               onClick={clicOptionD}
+               >OPTION D : {countriesSelected[3]?.name?.common}</button><br/>
+               <br/> 
+               <br/>
+               <button className='next'>NEXT QUESTION</button>
+               </section>
+           
+           
+           
+           : 'LOADING....'   }
+           
+      
+
+          </>
   )
 }
 
